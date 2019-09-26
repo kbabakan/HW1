01b768d92cad3dd706c5192824b51a773a394671
Kayhan Babakan <kayhanbabakan@Kayhans-MacBook-Pro.local>
2019-09-25 20:22:59 -0400

    v2

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 // take the name of the user and return it on the site
- function SumValue(){
+
+function SumValue(){
     var variable1 = +document.getElementById("input1").value;
     var variable2 = +document.getElementById("input2").value;
     var ifoper = document.getElementById("operation").value;
-    console.log(ifoper);
-
-    if(ifoper == "Addition"){
-        document.getElementById('Results').innerText = variable1+variable2;}
-    else if(ifoper == "Multiplication"){
-        document.getElementById('Results').innerText = variable1*variable2;}
-    else if(ifoper == "Subtraction"){
-        document.getElementById('Results').innerText = variable1-variable2;}    
-    else if(ifoper == "Division"){
-        document.getElementById('Results').innerText = variable1/variable2;}
-}
-
-var count = 0
-
-function counter(){
-    count += 1; 
-    console.log(count);
-    document.getElementById('counteroutput').innerText = count;
-}
-
-
-const x =  function SumValue(){
-    var variable1 = +document.getElementById("input1").value;
-    var variable2 = +document.getElementById("input2").value;
-    var ifoper = document.getElementById("operation").value;
-    console.log(ifoper);
+    var sign = "";
 
     if(ifoper == "Addition"){
         document.getElementById('Results').innerText = variable1+variable2;}
@@ -39,6 +15,44 @@ const x =  function SumValue(){
     else if(ifoper == "Division"){
         document.getElementById('Results').innerText = variable1/variable2;}
     
-        console.log(x)
+    if(ifoper == "Addition"){sign = ("+");}
+    else if(ifoper == "Multiplication"){
+        sign = ("x")}
+    else if(ifoper == "Subtraction"){
+        sign = ("-")}    
+    else if(ifoper == "Division"){
+        sign = ("/");}
+   
+        console.log(variable1+" "+sign+" "+variable2+" = "+document.getElementById('Results').innerText);
+        var log = [variable1, sign, variable2]
+        console.log(log)
+    }
+
+var count = 0
+
+function newFunction() {
+    var arr = [];
+    arr.push(document.getElementById("operation").innerText);
+    console.log(arr);
 }
 
+function counter(){
+    count += 1; 
+    document.getElementById('counteroutput').innerText = count;
+}
+
+const x =  function SumValue(){
+    var variable1 = +document.getElementById("input1").value;
+    var variable2 = +document.getElementById("input2").value;
+    var ifoper = document.getElementById("operation").value;
+
+    if(ifoper == "Addition"){
+        document.getElementById('Results').innerText = variable1+variable2;}
+    else if(ifoper == "Multiplication"){
+        document.getElementById('Results').innerText = variable1*variable2;}
+    else if(ifoper == "Subtraction"){
+        document.getElementById('Results').innerText = variable1-variable2;}    
+    else if(ifoper == "Division"){
+        document.getElementById('Results').innerText = variable1/variable2;}
+        
+}
